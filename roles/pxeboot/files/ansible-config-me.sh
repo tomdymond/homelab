349595577b035864cbcc0ecb9fac666c012d61b6
Tom Dymond <tom@kmem.org>
2018-01-21 21:28:03 +0000

    wip

--- roles/pxeboot/files/ansible-config-me.sh
+++ roles/pxeboot/files/ansible-config-me.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-runuser -l ansible -c 'ansible-pull -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git default-server.yml --purge &gt;&gt; /home/ansible/run.log'
-
+runuser -l ansible -c 'ansible-pull -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git default-server.yml >> /home/ansible/run.log'
 
 systemctl disable ansible-config-me.service
