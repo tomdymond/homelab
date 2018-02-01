e222b83e6aeebf8b48f79b1ecef32b41851b3bd1
Tom Dymond <tom@kmem.org>
2018-02-01 13:16:52 +0000

    wip

--- _run.sh
+++ _run.sh
@@ -1,4 +1,15 @@
 eval $(ssh-agent)
 ssh-add id_rsa
 
-ansible-playbook -i hosts --vault-password-file=~/.ansible_vault_password pxe.yml $@
+case $1 in 
+  tunnel)
+    HOSTS_FILE="ansible/hosts-tunnel"
+    ;;
+  *)
+    HOSTS_FILE="ansible/hosts"
+    ;;
+esac
+
+
+echo ansible-playbook -i ${HOSTS_FILE} --vault-password-file=~/.ansible_vault_password ansible/pxe.yml $@
+
