2c759ec96eb0ed4f8ecfd816d4a05b9d128b7d37
Tom Dymond <tom@kmem.org>
2018-02-03 16:53:23 +0000

    wip

--- ansible/roles/pxeboot/files/ansible-config-me.sh
+++ ansible/roles/pxeboot/files/ansible-config-me.sh
@@ -3,4 +3,4 @@
 runuser -l ansible -c 'ansible-pull -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/default-server.yml >> /home/ansible/run.log'
 
 systemctl disable ansible-config-me.service
-reboot
+#reboot
