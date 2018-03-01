da6baf874f0174ac346cb1c313fddbf08358a69b
Tom Dymond <tom@kmem.org>
2018-03-01 18:36:04 +0000

    wip

---
+++ terraform/consul-experiments/main.tf
@@ -0,0 +1,12 @@
+module "vm" {
+  vsphere_server="192.168.1.254"
+  node_base_name="minio-experiments"
+  source="../source/vsphere-vm/"
+  guest_id="centos7_64Guest"
+  vsphere_password="${var.vsphere_password}"
+  vsphere_user="${var.vsphere_user}"
+  bootstrap_network="LAB2"
+  memory="4096"
+  vcpu="2"
+  deploy_stack="minio-stack"
+}
