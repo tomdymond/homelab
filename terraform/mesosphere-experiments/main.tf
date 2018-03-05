2e50204272e147fac53b74c98da1f2eede5831da
Tom Dymond <tom@kmem.org>
2018-03-05 20:35:23 +0000

    wip

--- terraform/mesosphere-experiments/main.tf
+++ terraform/mesosphere-experiments/main.tf
@@ -28,19 +28,3 @@ module "mesos_priv_agents" {
   master_count=1
 }
 
-module "mesos_public_agents" {
-  vsphere_server="192.168.1.254"
-  node_base_name="mesos-joiners"
-  source="../source/vsphere-vm/"
-  guest_id="centos7_64Guest"
-  vsphere_password="${var.vsphere_password}"
-  vsphere_user="${var.vsphere_user}"
-  bootstrap_network="LAB2"
-  memory="2048"
-  vcpu="2"
-  deploy_stack="mesosphere-stack"
-  user_variables="MESOSPHERE_ROLE=slave"
-  master_count=1
-}
-
-
