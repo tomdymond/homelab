88454b24069babe4918e97a302a39798346522ca
Tom Dymond <tom@kmem.org>
2018-03-05 20:34:14 +0000

    wip

--- terraform/mesosphere-experiments/main.tf
+++ terraform/mesosphere-experiments/main.tf
@@ -1,6 +1,6 @@
 module "mesos_master" {
   vsphere_server="192.168.1.254"
-  node_base_name="consul-boot"
+  node_base_name="mesos-boot"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
   vsphere_password="${var.vsphere_password}"
@@ -10,12 +10,12 @@ module "mesos_master" {
   vcpu="2"
   deploy_stack="mesosphere-stack"
   user_variables="MESOSPHERE_ROLE=master"
-  count=3
+  master_count=3
 }
 
 module "mesos_priv_agents" {
   vsphere_server="192.168.1.254"
-  node_base_name="consul-joiners"
+  node_base_name="mesos-joiners"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
   vsphere_password="${var.vsphere_password}"
@@ -23,14 +23,14 @@ module "mesos_priv_agents" {
   bootstrap_network="LAB2"
   memory="2048"
   vcpu="2"
-  deploy_stack="consul-stack"
-  user_variables="MESOSPHERE_ROLE=slave!MESOS_MASTERS=${module.mesos_master.*.host_ip}"
+  deploy_stack="mesosphere-stack"
+  user_variables="MESOSPHERE_ROLE=slave"
   master_count=1
 }
 
 module "mesos_public_agents" {
   vsphere_server="192.168.1.254"
-  node_base_name="consul-joiners"
+  node_base_name="mesos-joiners"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
   vsphere_password="${var.vsphere_password}"
@@ -38,8 +38,8 @@ module "mesos_public_agents" {
   bootstrap_network="LAB2"
   memory="2048"
   vcpu="2"
-  deploy_stack="consul-stack"
-  user_variables="MESOSPHERE_ROLE=slave!MESOS_MASTERS=${module.mesos_master.*.host_ip}"
+  deploy_stack="mesosphere-stack"
+  user_variables="MESOSPHERE_ROLE=slave"
   master_count=1
 }
 
