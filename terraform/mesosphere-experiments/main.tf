a42237a5de3609f9ea3f84e176fd9dfb841990d7
Tom Dymond <tom@kmem.org>
2018-03-06 21:23:10 +0000

    wip

--- terraform/mesosphere-experiments/main.tf
+++ terraform/mesosphere-experiments/main.tf
@@ -1,6 +1,21 @@
+module "mesos_bootstrap" {
+  vsphere_server="192.168.1.254"
+  node_base_name="mesos-bootstrap"
+  source="../source/vsphere-vm/"
+  guest_id="centos7_64Guest"
+  vsphere_password="${var.vsphere_password}"
+  vsphere_user="${var.vsphere_user}"
+  bootstrap_network="LAB2"
+  memory="4096"
+  vcpu="2"
+  deploy_stack="mesosphere-stack"
+  user_variables="MESOSPHERE_ROLE=bootstrap!MESOSPHERE_CLUSTER_NAME=tomtest!MESOSPHERE_RESOLVER=192.168.1.1!MESOS_MASTERS=${module.mesos_master.host_ip}!MESOS_AGENTS_PRIV=${module.mesos_slave.host_ip}"
+  master_count=1
+}
+
 module "mesos_master" {
   vsphere_server="192.168.1.254"
-  node_base_name="mesos-boot"
+  node_base_name="mesos-master"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
   vsphere_password="${var.vsphere_password}"
