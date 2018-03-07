9b1082f7cf7057c9adc4770076185136d6c6ddab
Tom Dymond <tom@kmem.org>
2018-03-07 14:23:40 +0000

    wip

--- terraform/mesosphere-experiments/main.tf
+++ terraform/mesosphere-experiments/main.tf
@@ -1,5 +1,5 @@
 module "mesos_bootstrap" {
-  vsphere_server="192.168.1.254"
+  vsphere_server="127.0.0.1:8443"
   node_base_name="mesos-bootstrap"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
@@ -14,7 +14,7 @@ module "mesos_bootstrap" {
 }
 
 module "mesos_master" {
-  vsphere_server="192.168.1.254"
+  vsphere_server="127.0.0.1:8443"
   node_base_name="mesos-master"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
@@ -23,13 +23,13 @@ module "mesos_master" {
   bootstrap_network="LAB2"
   memory="4096"
   vcpu="2"
-  deploy_stack="mesosphere-stack"
+  deploy_stack="mesosphere-bootstrap-discovery-stack"
   user_variables="MESOSPHERE_ROLE=master"
   master_count=3
 }
 
 module "mesos_slave" {
-  vsphere_server="192.168.1.254"
+  vsphere_server="127.0.0.1:8443"
   node_base_name="mesos-joiners"
   source="../source/vsphere-vm/"
   guest_id="centos7_64Guest"
@@ -38,7 +38,7 @@ module "mesos_slave" {
   bootstrap_network="LAB2"
   memory="2048"
   vcpu="2"
-  deploy_stack="mesosphere-stack"
+  deploy_stack="mesosphere-bootstrap-discovery-stack"
   user_variables="MESOSPHERE_ROLE=slave"
   master_count=1
 }
