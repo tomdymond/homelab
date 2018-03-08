6b1152ce942fc1c7eb864646bdb2be5195da68fd
Tom Dymond <tom@kmem.org>
2018-03-08 15:41:52 +0000

    wip

--- ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
+++ ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 DEPLOY_STACK=${1}
+LOGFILE=/tmp/ansible-mesosphere-deploy.out
 
 source /etc/boot_environment;
 eval export $(echo $USER_VARIABLES | tr '!' ' ');
 
-MAXWAIT=300
+MAXWAIT=900
 while true; do 
-  if [ -f /etc/bootstrap_ip ]; then
+  if [ -f /etc/bootstrap_ip ]; then"
     export DCOS_BOOTSTRAP=$(</etc/bootstrap_ip)
-    if curl --head http://${DCOS_BOOTSTRAP} >/dev/null 2>&1; then 
-      /usr/bin/ansible-pull -vvv --vault-password-file=/root/.ansible_vault_password -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/mesosphere-stack.yml | tee -a /tmp/ansible-mesosphere-deploy.out
+    echo "Found bootstrap ip: ${DCOS_BOOTSTRAP}"
+    if curl --head http://${DCOS_BOOTSTRAP} >/dev/null 2>&1; then
+      echo "Successfully connected to HTTP server on bootstrap node"
+      echo "Running ansible playbook. Logging output to ${LOGFILE}"
+      /usr/bin/ansible-pull -vvv --vault-password-file=/root/.ansible_vault_password -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/mesosphere-stack.yml | tee -a ${LOGFILE}
     fi 
     exit 0
   fi
   sleep 1
 done
-echo "Gave up waiting for bootstrap_ip or http server connection" | tee -a /tmp/mesosphere-deploy.log
+echo "Gave up waiting for bootstrap_ip or http server connection" | tee -a ${LOGFILE}
 
 exit 1
