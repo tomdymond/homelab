0c27ea8709ae16a5fd4c524a0870e6be562cdb77
Tom Dymond <tom@kmem.org>
2018-03-08 14:42:09 +0000

    wip

--- ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
+++ ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
@@ -5,11 +5,17 @@ DEPLOY_STACK=${1}
 source /etc/boot_environment;
 eval export $(echo $USER_VARIABLES | tr '!' ' ');
 
+function boostrap_http_up() {
+  
+}
+
 MAXWAIT=300
 while true; do 
   if [ -f /etc/bootstrap_ip ]; then
     export DCOS_BOOTSTRAP=$(</etc/bootstrap_ip)
-    /usr/bin/ansible-pull -vvv --vault-password-file=/root/.ansible_vault_password -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/mesosphere-stack.yml | tee -a /tmp/ansible-mesosphere-deploy.out
+    if curl --head http://${DCOS_BOOTSTRAP}; then 
+      /usr/bin/ansible-pull -vvv --vault-password-file=/root/.ansible_vault_password -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/mesosphere-stack.yml | tee -a /tmp/ansible-mesosphere-deploy.out
+    fi 
     exit 0
   fi
   sleep 1
