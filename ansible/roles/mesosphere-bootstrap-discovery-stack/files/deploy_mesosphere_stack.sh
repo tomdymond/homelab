81eee16b9dd7b906f4dc4b07eacaf8012d5a0aba
Tom Dymond <tom@kmem.org>
2018-03-08 15:38:20 +0000

    wip

--- ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
+++ ansible/roles/mesosphere-bootstrap-discovery-stack/files/deploy_mesosphere_stack.sh
@@ -9,12 +9,13 @@ MAXWAIT=300
 while true; do 
   if [ -f /etc/bootstrap_ip ]; then
     export DCOS_BOOTSTRAP=$(</etc/bootstrap_ip)
-    if curl --head http://${DCOS_BOOTSTRAP}; then 
+    if curl --head http://${DCOS_BOOTSTRAP} >/dev/null 2>&1; then 
       /usr/bin/ansible-pull -vvv --vault-password-file=/root/.ansible_vault_password -C master -d /home/ansible/deploy -i /home/ansible/hosts -U https://github.com/tomdymond/homelab.git ansible/mesosphere-stack.yml | tee -a /tmp/ansible-mesosphere-deploy.out
     fi 
     exit 0
   fi
   sleep 1
 done
+echo "Gave up waiting for bootstrap_ip or http server connection" | tee -a /tmp/mesosphere-deploy.log
 
 exit 1
