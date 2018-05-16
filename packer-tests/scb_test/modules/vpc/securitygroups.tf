9274af70e67b4a72c4c1fec81cad5df2b15d9ebc
Tom Dymond <tom@kmem.org>
2018-05-16 17:20:31 +0100

    Remove SSH rules

--- packer-tests/scb_test/modules/vpc/securitygroups.tf
+++ packer-tests/scb_test/modules/vpc/securitygroups.tf
@@ -62,37 +62,42 @@ resource "aws_security_group_rule" "allow_app_to_any" {
   security_group_id = "${aws_security_group.secgroup_app.id}"
 }
 
-resource "aws_security_group_rule" "allow_any_to_web_ssh" {
-  description       = "Allow ANY to WEB port 22"
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.secgroup_web.id}"
-}
+# DEBUG RULE. REMOVE. USE BASTION HOST
+#resource "aws_security_group_rule" "allow_any_to_web_ssh" {
+#  description       = "Allow ANY to WEB port 22"
+#  type              = "ingress"
+#  from_port         = 22
+#  to_port           = 22
+#  protocol          = "tcp"
+#  cidr_blocks       = ["0.0.0.0/0"]
+#  security_group_id = "${aws_security_group.secgroup_web.id}"
+#}
+# DEBUG RULE. REMOVE
 
-# DEBUG. REMOVE
-resource "aws_security_group_rule" "allow_web_to_db_ssh" {
-  description              = "Allow WEB to DB port 22"
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.secgroup_web.id}"
-  security_group_id        = "${aws_security_group.secgroup_db.id}"
-}
 
-# DEBUG. REMOVE
-resource "aws_security_group_rule" "allow_web_to_app_ssh" {
-  description              = "Allow WEB to APP port 22"
-  type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
-  protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.secgroup_web.id}"
-  security_group_id        = "${aws_security_group.secgroup_app.id}"
-}
+# DEBUG RULE. REMOVE.
+#resource "aws_security_group_rule" "allow_web_to_db_ssh" {
+#  description              = "Allow WEB to DB port 22"
+#  type                     = "ingress"
+#  from_port                = 22
+#  to_port                  = 22
+#  protocol                 = "tcp"
+#  source_security_group_id = "${aws_security_group.secgroup_web.id}"
+#  security_group_id        = "${aws_security_group.secgroup_db.id}"
+#}
+# DEBUG RULE. REMOVE.
+
+# DEBUG RULE. REMOVE.
+#resource "aws_security_group_rule" "allow_web_to_app_ssh" {
+#  description              = "Allow WEB to APP port 22"
+#  type                     = "ingress"
+#  from_port                = 22
+#  to_port                  = 22
+#  protocol                 = "tcp"
+#  source_security_group_id = "${aws_security_group.secgroup_web.id}"
+#  security_group_id        = "${aws_security_group.secgroup_app.id}"
+#}
+# DEBUG RULE. REMOVE.
 
 resource "aws_security_group_rule" "allow_web_from_elb" {
   description              = "Allow WEB from ELB port 80"
